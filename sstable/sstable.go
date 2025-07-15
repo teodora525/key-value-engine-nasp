08b6df199f5073a7c8a9b1b7b8ffdf49b91b65ee
Teodora Nikolic <tea.nikolic525@gmail.com>
2025-07-15 10:44:38 +0200

    Update - dodato merkle stablo

--- sstable/sstable.go
+++ sstable/sstable.go
@@ -2,15 +2,16 @@ package sstable
 
 import (
 	"encoding/binary"
+	"fmt"
 	"key-value-engine/config"
 	"key-value-engine/types"
+	"key-value-engine/wal"
 	"os"
 	"sort"
 )
 
-// Kreira SSTable: .data, .index, .summary, .filter
+// Glavna funkcija za kreiranje SSTable fajlova
 func CreateSSTable(path string, entries []*types.Entry) error {
-	// 1. Sortiraj po ključu
 	sort.Slice(entries, func(i, j int) bool {
 		return string(entries[i].Key) < string(entries[j].Key)
 	})
@@ -30,18 +31,16 @@ func CreateSSTable(path string, entries []*types.Entry) error {
 	var offset uint64 = 0
 	var indexEntries [][]byte
 
-	// 2. Inicijalizuj Bloom Filter
 	bloom := NewBloomFilter(1024, 3)
 
-	// 3. Upis u data + priprema index
 	for _, entry := range entries {
 		serialized := entry.Serialize()
+
 		_, err := dataFile.Write(serialized)
 		if err != nil {
 			return err
 		}
 
-		// upiši key, offset u index
 		keySize := uint64(len(entry.Key))
 		buf := make([]byte, 8+keySize+8)
 		binary.LittleEndian.PutUint64(buf[0:8], keySize)
@@ -55,20 +54,30 @@ func CreateSSTable(path string, entries []*types.Entry) error {
 		}
 
 		offset += uint64(len(serialized))
-
-		// dodaj u bloom filter
 		bloom.Add(entry.Key)
 	}
 
-	// 4. Sačuvaj Bloom Filter
+	// Snimi Bloom filter
 	err = bloom.SaveToFile(path + ".filter")
 	if err != nil {
 		return err
 	}
 
-	// 5. Sačuvaj Summary
+	// Snimi Summary fajl
 	cfg := config.LoadConfig("config.json")
 	err = writeSummary(path, indexEntries, cfg.SummaryStep)
+	if err != nil {
+		return err
+	}
+
+	// Merkle stablo
+	var values [][]byte
+	for _, entry := range entries {
+		values = append(values, entry.Value)
+	}
+
+	tree := NewMerkleTree(values)
+	err = os.WriteFile(path+".meta", tree.Root.Hash, 0644)
 	if err != nil {
 		return err
 	}
@@ -76,7 +85,7 @@ func CreateSSTable(path string, entries []*types.Entry) error {
 	return nil
 }
 
-// Summary fajl — upisuje svaki N-ti index zapis
+// Summary fajl sadrži svaki N-ti zapis iz Index fajla
 func writeSummary(path string, indexEntries [][]byte, step int) error {
 	summaryFile, err := os.Create(path + ".summary")
 	if err != nil {
@@ -90,6 +99,40 @@ func writeSummary(path string, indexEntries [][]byte, step int) error {
 		if err != nil {
 			return err
 		}
+	}
+
+	return nil
+}
+
+// Verifikacija Merkle stabla za datu SSTable instancu
+func VerifySSTable(path string) error {
+	f, err := os.Open(path + ".data")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	var values [][]byte
+	for {
+		entry, err := wal.ReadOneEntry(f)
+		if err != nil {
+			break
+		}
+		values = append(values, entry.Value)
+	}
+
+	tree := NewMerkleTree(values)
+	computed := tree.Root.Hash
+
+	expected, err := os.ReadFile(path + ".meta")
+	if err != nil {
+		return err
+	}
+
+	if string(computed) == string(expected) {
+		fmt.Println("✅ Merkle validacija uspešna!")
+	} else {
+		fmt.Println("❌ Merkle NE validna! Podaci su izmenjeni.")
 	}
 
 	return nil
