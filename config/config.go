11f657731d8e5d261550d8e96e75d7a0721d1704
Teodora Nikolic <tea.nikolic525@gmail.com>
2025-07-15 10:15:18 +0200

    Update config

--- config/config.go
+++ config/config.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Config struct {
+	MemtableMaxSize int    `json:"memtable_max_size"`
+	BlockSizeKB     int    `json:"block_size_kb"`
+	CacheSize       int    `json:"cache_size"`
+	SummaryStep     int    `json:"summary_step"`
+	MemtableType    string `json:"memtable_type"`
+}
+
+var DefaultConfig = Config{
+	MemtableMaxSize: 1000,
+	BlockSizeKB:     4,
+	CacheSize:       128,
+	SummaryStep:     5,
+	MemtableType:    "hashmap",
+}
+
+// Učitavanje iz config fajla
+func LoadConfig(path string) Config {
+	file, err := os.Open(path)
+	if err != nil {
+		return DefaultConfig
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	config := DefaultConfig
+	err = decoder.Decode(&config)
+	if err != nil {
+		return DefaultConfig
+	}
+
+	return config
+}
